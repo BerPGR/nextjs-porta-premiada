3713225e0204cdf9eacc03fe9a7545fede1724d7
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-05-26 15:21:57 -0300

    Create porta component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,12 @@
+import Porta from "@/components/Porta";
 import Presente from "@/components/Presente";
 
 export default function Home() {
   return (
-    <div>
-      <Presente />
+    <div style={{display: 'flex'}}>
+      <Porta selecionada />
+      <Porta />
+      {/*<Presente />*/}
     </div>
   )
 }
