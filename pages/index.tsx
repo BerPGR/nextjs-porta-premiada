2bf70078727f6d94fc6eaeb6fac09379a03e8aa6
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-06-01 11:02:35 -0300

    Array com portas

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,26 @@ import PortaModel from "../model/porta";
 import Porta from "../components/Porta";
 import Presente from "../components/Presente";
 import { useState } from "react";
+import { atualizarPortas, criarPortas } from "../functions/portas";
 
 export default function Home() {
 
-  const [p1, setP1] = useState(new PortaModel(1))
+  const [portas, setPortas] = useState(criarPortas(3, 2))
 
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return (
+        <Porta value={porta} key={porta.numero} 
+        onChange={novaPorta => {
+          setPortas(atualizarPortas(portas, novaPorta))
+        }} />
+      )
+    })
+  }
 
   return (
     <div style={{display: 'flex'}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+      {renderizarPortas()}
       {/*<Presente />*/}
     </div>
   )
